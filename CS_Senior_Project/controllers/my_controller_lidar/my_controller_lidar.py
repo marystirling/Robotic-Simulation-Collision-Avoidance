09ec9924525ab5459d92da6c8dce62e703c0edd7
marystirling <marystirlingbrown@gmail.com>
2022-02-26 13:38:56 -0600

    retrieves coordinate position and distance from an object with lidar

--- CS_Senior_Project/controllers/my_controller_lidar/my_controller_lidar.py
+++ CS_Senior_Project/controllers/my_controller_lidar/my_controller_lidar.py
@@ -3,8 +3,12 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+from controller import Lidar
+from controller import LidarPoint
 
-def run_robot(robot):
+
+
+def run_robot(robot, my_lidar):
     # get the time step of the current world.
     timestep = 32
     max_speed = 6.28
@@ -18,8 +22,8 @@ def run_robot(robot):
     left_motor = robot.getDevice('left wheel motor')
     right_motor = robot.getDevice('right wheel motor')
     
-    left_motor.setPosition(10.0)
-    right_motor.setPosition(10.0)
+    left_motor.setPosition(20.0)
+    right_motor.setPosition(20.0)
     
     
     left_motor.setVelocity(0.0)
@@ -28,6 +32,10 @@ def run_robot(robot):
     # Lidar
     lidar = robot.getLidar('lidar')
     lidar.enable(timestep)
+    lidar.enablePointCloud()
+    
+    
+    
     
     # Main loop:
     # - perform simulation steps until Webots is stopping the controller
@@ -35,8 +43,15 @@ def run_robot(robot):
         # Read the sensors:
         # Enter here functions to read sensor data, like:
         #  val = ds.getValue()
+        lidarPoints = lidar.getPointCloud(); # Get the point cloud
+
+    # Print out the x, y, z, and layer information for the first point in the point cloud    
+        print("x: " + str(lidarPoints[0].x) + " y: " + str(lidarPoints[0].y) + " z: " + str(lidarPoints[0].z) + " layer: " + str(lidarPoints[0].layer_id))
         range_image = lidar.getRangeImage()
-        print("{}".format(range_image))
+        #print(my_lidar.x)
+        #point_tuple = (my_lidar.x, my_lidar.y, my_lidar.z, my_lidar.layer_id, my_lidar.time)
+        #print(point_tuple)
+        print("{}".format(range_image[0]))
     
         # Process sensor data here.
     
@@ -51,4 +66,5 @@ def run_robot(robot):
 if __name__ == "__main__":
 # create the Robot instance.
     my_robot = Robot()
-    run_robot(my_robot)
+    my_lidar = LidarPoint()
+    run_robot(my_robot, my_lidar)
