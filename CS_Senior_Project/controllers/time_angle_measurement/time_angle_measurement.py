227c83e1e9d2968e5698796fd08f48a59ee942c8
marystirling <marystirlingbrown@gmail.com>
2022-03-30 22:02:10 -0500

    turn robot to the left

--- CS_Senior_Project/controllers/time_angle_measurement/time_angle_measurement.py
+++ CS_Senior_Project/controllers/time_angle_measurement/time_angle_measurement.py
@@ -1,32 +1,54 @@
-"""time_angle_measurement controller."""
+"""my_controller_lidar controller."""
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+from controller import Lidar
+import math
 
+
+
+def run_robot(robot):
+    # get the time step of the current world.
+    timestep = 32
+    max_speed = 6.28
+    # You should insert a getDevice-like function in order to get the
+    # instance of a device of the robot. Something like:
+    #  motor = robot.getDevice('motorname')
+    #  ds = robot.getDevice('dsname')
+    #  ds.enable(timestep)
+    
+    # Motors
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
+    
+    left_motor.setPosition(20.0)
+    right_motor.setPosition(20.0)
+    
+    
+    left_motor.setVelocity(0.0)
+    right_motor.setVelocity(0.0)
+    
+    
+  
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(timestep) != -1:
+        
+
+      
+        # Enter here functions to send actuator commands, like:
+        #  motor.setPosition(10.0)
+        #left_motor.setVelocity(max_speed*0.25)
+        #right_motor.setVelocity(max_speed*0.25)
+        
+       #left_motor.setVelocity(max_speed)
+       # time = time.time()
+        
+
+
+if __name__ == "__main__":
 # create the Robot instance.
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+    my_robot = Robot()
+    #my_lidar = LidarPoint()
+    run_robot(my_robot)
